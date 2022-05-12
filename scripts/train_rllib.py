80893a1263ee0141a5dc6a93d4bb3db20fa87537
emasquil <eliasmasquil@gmail.com>
2022-05-12 14:34:49 +0200

    add config files

--- scripts/train_rllib.py
+++ scripts/train_rllib.py
@@ -4,16 +4,13 @@ from copy import deepcopy
 import click
 import imgc_marl.envs.multiagent as multiagent
 import imgc_marl.envs.single_agent as single_agent
-from imgc_marl.callbacks import MultiGoalMultiAgentCallbacks
 import numpy as np
+import yaml
+from imgc_marl.callbacks import MultiGoalMultiAgentCallbacks
 from imgc_marl.utils import after_training_eval_rllib
 from ray.rllib.agents.ppo import DEFAULT_CONFIG, PPOTrainer
 from ray.rllib.policy.policy import PolicySpec
 from ray.tune.logger import pretty_print
-
-SEED = 42
-TRAINING_STEPS = 500
-NUM_WORKERS = 8
 
 
 @click.command()
@@ -24,32 +21,37 @@ NUM_WORKERS = 8
         case_sensitive=True,
     ),
 )
-def train(environment):
+@click.argument("config")
+def train(environment, config):
     """Training loop using RLlib"""
-    random.seed(SEED)
-    np.random.seed(SEED)
-    # Configure the algorithm.
-    config = DEFAULT_CONFIG.copy()
-    config["num_workers"] = NUM_WORKERS
-    config["framework"] = "torch"
-    config["seed"] = SEED
-    multiagent_flag = True
-    goal_dict = None
 
-    # Train
+    # Loading user config
+    with open(config, "r") as f:
+        user_config = yaml.safe_load(f)
+    # Seeding everything
+    seed = user_config.get("seed", random.randint(0, 1e6))
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # General settings of the algorithm
+    config = DEFAULT_CONFIG.copy()
+    config["num_workers"] = user_config["training"].get("num_workers", 0)
+    config["framework"] = "torch"
+    config["seed"] = seed
+
+    # Particular settings dependent on the environment
     if environment == "single_agent":
         multiagent_flag = False
+        goal_dict = None
         config["horizon"] = single_agent.SIMPLE_TIMELIMIT
-        # Create our RLlib Trainer.
         trainer = PPOTrainer(config=config, env=single_agent.SimpleEnv)
         eval_env = single_agent.SimpleEnv()
 
     elif environment == "basic_marl":
+        multiagent_flag = True
+        goal_dict = None
         config["horizon"] = multiagent.SIMPLE_TIMELIMIT
-        config["env_config"] = {
-            "n_agents": 2,
-            "continuous": True,
-        }
+        config["env_config"] = user_config["env_config"]
         config["multiagent"] = {
             "policies": {
                 "agent_0": PolicySpec(
@@ -67,10 +69,14 @@ def train(environment):
         eval_env = multiagent.OneBoxEnv(config["env_config"])
 
     elif environment == "goal_lines":
-        goal_dict = {"agent_0": list(range(6)), "agent_1": list(range(6))}
-        config["callbacks"] = MultiGoalMultiAgentCallbacks
+        multiagent_flag = True
+        goal_dict = {
+            "agent_0": list(range(multiagent.N_GOAL_LINES)),
+            "agent_1": list(range(multiagent.N_GOAL_LINES)),
+        }
         config["horizon"] = multiagent.GOAL_LINES_TIMELIMIT
-        config["env_config"] = {"continuous": True, "centralized": True}
+        config["env_config"] = user_config["env_config"]
+        config["callbacks"] = MultiGoalMultiAgentCallbacks
         config["multiagent"] = {
             "policies": {
                 "agent_0": PolicySpec(
@@ -88,10 +94,10 @@ def train(environment):
         trainer = PPOTrainer(config=config, env=multiagent.GoalLinesEnv)
         eval_env = multiagent.GoalLinesEnv(eval_config["env_config"])
 
-    # Run it for n training iterations. A training iteration includes
+    # Train for training_steps iterations. A training iteration includes
     # parallel sample collection by the environment workers as well as
     # loss calculation on the collected batch and a model update.
-    for _ in range(TRAINING_STEPS):
+    for _ in range(user_config["training"]["training_steps"]):
         result = trainer.train()
         print(pretty_print(result))
 
