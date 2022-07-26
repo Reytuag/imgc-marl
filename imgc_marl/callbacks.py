fix basic naming game: comm matrix as torch tensor + softmax instead of clipping

imgc_marl/callbacks.py
@@ -711,10 +711,17 @@ class LargeGoalLinesBasicNamingGame(LargeGoalLinesCallback):
         leader_goal = base_env.envs[0].goal_space[leader_goal_index]
         if np.random.random() > 0.5:
             # agent 0 lead
-            scores = policies["agent_0"].model._communication_matrix[leader_goal_index]
-            follower_goal_index = np.random.choice(
-                np.flatnonzero(scores == scores.max())
-            )
+            with torch.no_grad():
+                scores = (
+                    torch.nn.functional.softmax(
+                        policies["agent_0"].model._communication_matrix[
+                            leader_goal_index
+                        ]
+                    )
+                    .detach()
+                    .numpy()
+                )
+            follower_goal_index = np.random.choice(range(len(scores)), 1, p=scores)[0]
             follower_goal = base_env.envs[0].goal_space[follower_goal_index]
             agent_0_goal = leader_goal
             agent_1_goal = follower_goal
@@ -726,10 +733,17 @@ class LargeGoalLinesBasicNamingGame(LargeGoalLinesCallback):
             }
         else:
             # agent 1 lead
-            scores = policies["agent_1"].model._communication_matrix[leader_goal_index]
-            follower_goal_index = np.random.choice(
-                np.flatnonzero(scores == scores.max())
-            )
+            with torch.no_grad():
+                scores = (
+                    torch.nn.functional.softmax(
+                        policies["agent_1"].model._communication_matrix[
+                            leader_goal_index
+                        ]
+                    )
+                    .detach()
+                    .numpy()
+                )
+            follower_goal_index = np.random.choice(range(len(scores)), 1, p=scores)[0]
             follower_goal = base_env.envs[0].goal_space[follower_goal_index]
             agent_1_goal = leader_goal
             agent_0_goal = follower_goal

imgc_marl/models/basic_naming_game.py
@@ -136,20 +136,14 @@ class BasicNamingNetwork(TorchModelV2, nn.Module):
         self._last_flat_in = None
 
         # Custom communication branch
-        # self._communication_matrix = nn.Parameter(
-        #     torch.zeros(
-        #         [
-        #             model_config["custom_model_config"]["number_of_goals"],
-        #             model_config["custom_model_config"]["number_of_goals"],
-        #         ]
-        #     ),
-        #     requires_grad=True,
-        # )
-        self._communication_matrix = np.zeros(
-            [
-                model_config["custom_model_config"]["number_of_goals"],
-                model_config["custom_model_config"]["number_of_goals"],
-            ]
+        self._communication_matrix = nn.Parameter(
+            torch.zeros(
+                [
+                    model_config["custom_model_config"]["number_of_goals"],
+                    model_config["custom_model_config"]["number_of_goals"],
+                ]
+            ),
+            requires_grad=True,
         )
 
     @override(TorchModelV2)

imgc_marl/policies/basic_naming_game.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Dict, List, Type, Union
 
+import numpy as np
 import ray
 from ray.rllib.agents.ppo import PPOTorchPolicy, PPOTrainer
 from ray.rllib.agents.ppo.ppo_tf_policy import setup_config
@@ -25,9 +26,9 @@ from ray.rllib.utils.torch_utils import (
     sequence_mask,
 )
 from ray.rllib.utils.typing import TensorType
-import numpy as np
 
-DELTA = 0.1 / 30
+DELTA = 0.1 / (30 * 60)
+# scaling factor: 30 iterations x 60 episodes each agent will lead (60 games/training it)
 
 torch, nn = try_import_torch()
 
@@ -152,11 +153,14 @@ class BasicNamingPolicy(PPOTorchPolicy):
                     leader_goal = m["leader_goal"]
                     follower_goal = m["follower_goal"]
                     if train_batch["rewards"][i]:
-                        model._communication_matrix[leader_goal, :] -= DELTA
-                        model._communication_matrix[leader_goal, follower_goal] += (
-                            2 * DELTA
-                        )
+                        with torch.no_grad():
+                            model._communication_matrix[leader_goal, :] -= DELTA
+                            model._communication_matrix[leader_goal, follower_goal] += (
+                                2 * DELTA
+                            )
                     else:
-                        model._communication_matrix[leader_goal, follower_goal] -= DELTA
-                    np.clip(model._communication_matrix, 0.0, 1.0)
+                        with torch.no_grad():
+                            model._communication_matrix[
+                                leader_goal, follower_goal
+                            ] -= DELTA
         return total_loss