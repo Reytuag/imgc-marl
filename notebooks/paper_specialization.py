d0d1799ef40281fa79acfb046c86952d7841cb55
Eleni <e.nisioti@essex.ac.uk>
2022-10-28 22:14:32 +0200

    plotted for aamas

--- notebooks/paper_specialization.py
+++ notebooks/paper_specialization.py
@@ -21,22 +21,26 @@ import seaborn
 import math
 import pickle
 # In[18]:
+
+
+cm = 1 / 2.54  # for converting inches to cm
+fig_size = (10.48 * cm, 6 * cm)
 params = {'legend.fontsize': 6,
                   "figure.autolayout": True,
                   'font.size': 8,
                   'pdf.fonttype':42,
-                  'ps.fonttype':42}
+                  'ps.fonttype':42,
+          "figure.figsize": fig_size}
 plt.rcParams.update(params)
-
-cm = 1 / 2.54  # for converting inches to cm
-fig_size = (10.48 * cm, 6 * cm)
-plt.figure(figsize=fig_size)
+#plt.figure(figsize=fig_size)
 
 NUMBER_OF_LANDMARKS = 6
 n_agents = 2
 #results_dir = os.environ["SCRATCH"]+"/elias_expe/2_agents/cooperative/modified_reward/6_landmarks/independent"
-results_dirs = {"3_landmarks": "/media/elena/LaCie/aamas_2023/paper/align_3_landmarks",
-              "6_landmarks": "/media/elena/LaCie/aamas_2023/paper/align_6_landmarks"}
+results_dirs = {"3_landmarks": "/media/elena/LaCie/aamas_2023/paper/alignment/3_landmarks_reward2",
+              "6_landmarks": "/media/elena/LaCie/aamas_2023/paper/alignment/6_landmarks_reward2"}
+results_dirs = {
+              "6_landmarks": "/media/elena/LaCie/aamas_2023/paper/alignment/6_landmarks_reward2"}
 #results_dir = "/media/elena/LaCie/aamas_2023/paper/align_6_landmarks/independent"
 #results_dir = "/media/elena/LaCie/elias_expe/2_agents/all/modified_reward/6_landmarks/centralized"
 
@@ -67,9 +71,29 @@ df = pd.DataFrame([["3 landmarks", "independent", independent_3[0]],
                    ["6 landmarks", "naming", naming_6[1]]],
                  columns = ["landmarks", "method", "special"])
 """
+labels_to_print = {"0align": "$0%$-align",
+"centralized": "$100%$-align",
+                   "independent": "$0%$-align",
+
+                   "50align": "$50%$-align",
+                   "75align": "$75%$-align",
+                   "25align": "$25%$-align",
+                  "100align": "$100%$-align",
+                  "naming_game_30msg_alpha10_temp30": "GC-game"}
+labels_to_print = {"0align": "$0\%$-align",
+                   "independent": "$0\%$-align",
+                   "centralized": "$100\%$-align",
+                   "75align": "$75\%$-align",
+                   "25align": "$25\%$-align",
+                   "50align": "$50\%$-align",
+                   "100align": "$100\%$-align",
+                   "naming_game_30msg_alpha10_temp30": "GC-game"}
 
 df = pd.DataFrame(columns= ["landmarks", "method", "special"])
 
+# In[ ]:
+
+"""
 for landmarks, methods_dir in results_dirs.items():
     print(landmarks, methods_dir)
     NUMBER_OF_LANDMARKS = int(landmarks[0])
@@ -79,16 +103,10 @@ for landmarks, methods_dir in results_dirs.items():
     goals_index = {i: g for i, g in zip(range(len(goals)), goals)}
     agents = [f"agent_{i}" for i in range(n_agents)]
     results_dir =  [os.path.join(methods_dir, o) for o in os.listdir(methods_dir) if os.path.isdir(methods_dir + "/" + o)]
-    for method in results_dir:
-        print("method is", method)
-        if "independent" in method:
-            method_name = "independent"
-        if "centralized" in method:
-            method_name = "centralized"
-        if "game" in method:
-            method_name = "coordination game"
-        if "50align" not in method:
-            print("method_name is",method_name)
+    methods_names =  [o for o in os.listdir(methods_dir) if os.path.isdir(methods_dir + "/" +    o)]
+
+    for idx,method in enumerate(results_dir):
+            method_name = labels_to_print[methods_names[idx]]
 
             specializations_during_training = []
             specializations_convergence = []
@@ -103,7 +121,6 @@ for landmarks, methods_dir in results_dirs.items():
                         # we always redefine this to only consider the last set of results (last evaluation)
                         metrics = json.loads(result).get("evaluation")
                         if metrics is not None:
-                            print("loading")
                             for g in goals:
                                 for agent in agents:
                                     if (
@@ -118,14 +135,14 @@ for landmarks, methods_dir in results_dirs.items():
                                         specializations_during_training.append(
                                             aux.value_counts().max() / aux.value_counts().sum()
                                         )
-                        special = np.mean(specializations_during_training[-len(goals) * n_agents :])
-                        print(specializations_during_training[-len(goals) * n_agents :])
+                    special = np.mean(specializations_during_training[-len(goals) * n_agents :])
+                    print(specializations_during_training[-len(goals) * n_agents :])
 
 
-                        if special != None and not math.isnan(special):
-                            new_row = {"landmarks": landmarks, "method": method_name, "special": special}
-                            print(new_row)
-                            df = df.append(new_row, ignore_index=True)
+                    if special != None and not math.isnan(special):
+                        new_row = {"landmarks": landmarks, "method": method_name, "special": special}
+                        print(new_row)
+                        df = df.append(new_row, ignore_index=True)
 
             
         
@@ -133,23 +150,40 @@ for landmarks, methods_dir in results_dirs.items():
 
 # view data
 print(df)
+"""
 
 
 
 
-
-
-
-# In[ ]:
-
-
+with open("specialization.pkl", "rb") as f:
+    df = pickle.load( f)
+#ax = df.plot.bar( x="method",y="special")
+print(df)
 # plot grouped bar chart
-g=seaborn.catplot(data=df,kind="bar",x='landmarks', hue="method",y="special")
-g.despine(left=True)
-g.set_axis_labels("", "Specialization")
-g.legend.set_title("")
-plt.savefig("special.pdf")
-plt.savefig("special.png")
+#axs = seaborn.set(rc={'figure.figsize':fig_size})
+correct_order = ["$0\%$-align", "$50\%$-align", "$100\%$-align","GC-game" ]
+fig, axs = plt.subplots( 1,figsize=fig_size)
+
+seaborn.barplot(data=df, x="method",y="special", estimator=np.mean, ci=85, capsize=.2, order=correct_order,ax=axs)
+axs.set(xlabel="",ylabel="Specialization, $s$")
+#g.despine(left=True)
+#ax.set_xlabel("")
+#ax.set_ylabel("Specialization")
+
+#plt.set(xlabel="", ylabel="Specialization")
+#g.legend.set_title("")
+"""
+handles, labels = ax.get_legend_handles_labels()
+order = []
+for el in correct_order:
+    for label_idx, actual_label in enumerate(labels):
+        if el ==actual_label:
+            print("actual label", actual_label)
+            order.append(label_idx)
+ax.legend([handles[idx] for idx in order], [labels[idx] for idx in order])
+"""
+plt.savefig("special_cooperative.pdf")
+plt.savefig("special_cooperative.png")
 
 with open("specialization.pkl", "wb") as f:
     pickle.dump(df, f)
