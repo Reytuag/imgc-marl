f19b373ad511b4d7e78149db2f8342f320b334ad
Gautier Hamon <hamon.gautier.16@gmail.com>
2022-12-09 04:35:39 +0100

    centralized critic

--- imgc_marl/models/__init__.py
+++ imgc_marl/models/__init__.py
@@ -1,3 +1,4 @@
 from imgc_marl.models.basic_communication import BasicCommunicationNetwork
 from imgc_marl.models.full_communication import FullCommunicationNetwork
 from imgc_marl.models.basic_naming_game import BasicNamingNetwork
+from imgc_marl.models.centralized_critic_models import TorchCentralizedCriticModel
